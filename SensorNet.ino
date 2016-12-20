773af2aa9dc7f83a72dfe3bc7636066858b7bb0f
patrick <patrick@sernetz.com>
2016-12-20 21:58:51 +0100

    initial

---
+++ SensorNet.ino
@@ -0,0 +1,117 @@
+#include <Adafruit_Sensor.h>
+
+
+
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+ESP8266WebServer server(80);
+WiFiManager wifiManager;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp8266!");
+
+}
+
+void handleReset() {
+  WiFi.disconnect();
+  delay(3000);
+  ESP.reset();
+}
+
+void handleNotFound(){
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void setup() {
+
+  Serial.begin(115200);
+
+  delay(100);
+  Serial.println("Starting");
+
+  wifiManager.autoConnect("SENSOR-NET", "12345678");
+
+  // Set up mDNS responder:
+  // - first argument is the domain name, in this example
+  //   the fully-qualified domain name is "esp8266.local"
+  // - second argument is the IP address to advertise
+  //   we send our IP address on the WiFi network
+  if (!MDNS.begin("esp8266")) {
+    Serial.println("Error setting up MDNS responder!");
+    while(1) { 
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+
+  server.on("/", handleRoot);
+  server.on("/reset", handleReset);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("Web Server started");
+  dht.begin();
+
+}
+
+void loop() {
+  server.handleClient();
+
+  delay(2000);
+  
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+ 
+}
